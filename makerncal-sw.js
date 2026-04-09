d66d5fcead123078f5c75b92617e4cd683d051ca
mechanicalclub <mechanicaliotlab@gmail.com>
2026-04-09 22:16:55 +0800

    Add files via upload

---
+++ makerncal-sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'makerncal-v1';
+const ASSETS = [
+  './makerncal.html',
+  './makerncal-manifest.json',
+  './makerncal-192.png',
+  './makerncal-512.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok && e.request.method === 'GET') {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match('./makerncal.html')))
+  );
+});
